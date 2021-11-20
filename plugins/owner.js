83feba9f6ed9ac3f56136d6999de836410783e33
ANUSER <93968310+ANUSER1@users.noreply.github.com>
2021-11-20 10:27:11 +0530

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -18,7 +18,7 @@ if (Config.WORKTYPE == 'private') {
         }
 
         if (Config.PLK == 'default') {
-            await message.client.sendMessage(message.jid,'JINUSIR BOT CREATED BY *JINU*' , MessageType.text);
+            await message.client.sendMessage(message.jid,'ACE-V1 BOT CREATED BY *ANUSER😎*' , MessageType.text);
         }
         else {
             await message.client.sendMessage(message.jid,Config.PLK + '\n\n---------------------', MessageType.text);
@@ -36,7 +36,7 @@ else if (Config.WORKTYPE == 'public') {
         }
 
         if (Config.PLK == 'default') {
-            await message.client.sendMessage(message.jid,'jinusir Bot created by *jinu*' , MessageType.text);
+            await message.client.sendMessage(message.jid,'ACE-V1 CREATED BY ANUSER😎*' , MessageType.text);
         }
         else {
             await message.client.sendMessage(message.jid,Config.PLK + '*HE IS MY BOSS*', MessageType.text);
